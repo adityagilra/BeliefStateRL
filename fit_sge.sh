4e4cddef765a26364d839f3ba588558a90ac12dc
adityagilra <aditya_gilra@yahoo.com>
2022-03-13 19:17:59 +0000

    Added the fit_sge.sh file to run fit.py on the SGE / qsub in the background on SHARC at TUoS.

---
+++ fit_sge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Load the conda module
+module load apps/python/conda
+
+source activate pytorch_py3.6
+# Run the program <name>.py
+# nohup doesn't play well with qsub / qstat,
+#  doesn't show up in qstat, doesn't create out.txt
+#nohup python FOLLOW_forward_ff_rec_train.py > out.txt
+
+# if you don't redirect output, a <sh file name>.o<qsubid> file contains output
+# use -u for unbuffered stdout, i.e. flush after every print
+#  else output gets written only after a flush
+#  and no flush when a `qdel <qpid>` kill process is sent
+python -u fit.py > fit_out.txt
